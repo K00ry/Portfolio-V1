7710c0f050af63ec31bf69a24cd63b009bfc8ddd
kourosh mirzaei <Koory.yellow@gmail.com>
2016-10-01 21:14:07 -0400

    Layover for mobile : Done!

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ requirejs.config({
         tweenmax: 'TweenMax.min',
         jquery: 'jquery.min',
         scrollmagic: 'ScrollMagic.min',
+        indicator: 'debug.addIndicators.min',
         tether: 'tether',
         bootstrap: 'bootstrap.min',
         gsap: 'animation.gsap.min',
-        indicator: 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.3/plugins/debug.addIndicators',
         tweenlite: 'TweenLite',
         cssplugin: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/plugins/CSSPlugin.min',
         scrollto: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/plugins/ScrollToPlugin.min'
--- js/writen/main.js
+++ js/writen/main.js
@@ -25,17 +25,8 @@ define(['tweenmax', 'jquery', 'scrollmagic', 'tweenlite', 'scrollto'], function(
 
 
 
+    /////////////////smoooth Navigation\\\\\\\\\\\\\\\\\\\\\\
 
-    ////////////////////////
-
-    //   $('a[href^="#"]').click(function(event) {
-    // var id = $(this).attr("href");
-
-    // var target = $(id).offset().top ;
-
-    // $('html, body').animate({scrollTop:target}, 800);
-    // event.preventDefault();
-    // });
 
     var target_home = $('#home').offset().top,
         target_portfolio = $('#portfolio').offset().top,
@@ -62,17 +53,61 @@ define(['tweenmax', 'jquery', 'scrollmagic', 'tweenlite', 'scrollto'], function(
 
 
 
+    ///////////////// OVERLAY \\\\\\\\\\\\\\\\\\\\\\
+    // .css("display", "flex");
 
 
 
 
 
+    ////overlay data JSON
+
+    var data = [{
+        "name": "WEB_API",
+        "img": "img/work/tablet/1.png",
+        "info": "In This Project, using AJAX i used two Public API's(spotify/Flickr) to search for the artist and form a Gallery from the result,user then can choose and album and get latest pictures from Flickr related to the chosen album.",
+        "tech": ['HTML', 'SASS', 'JavaScript', 'Jquery', 'AJAX', 'GIT'],
+        "git": "https://k00ry.github.io/web_api_v1/"
+    }, {
+        "name": "WEB_APP",
+        "img": "img/work/tablet/2.png",
+        "info": "This project was about building a beautiful web application dashboard complete with JavaScript-driven charts and graphs base on a suplied graphic mockup. This was a front end project onlythat required to create the responsive layout in HTML and CSS with added JavaScript functionality. Flexbox and Sass proved to be invaluable tools.",
+        "tech": ['HTML', 'SASS', 'JavaScript', 'Jquery', 'GIT'],
+        "git": "https://k00ry.github.io/web_app_dashboard_v2/"
+    }];
+
+
+
+    var click_index;
+
+
+    $('.work-mobile').click(function() {
+
+        $('#overlay').addClass('open');
+        click_index = $(this).index();
+        $('#title').html(data[click_index].name);
+        $('#over-img').attr("src", data[click_index].img);
+        $('#info-text').html(data[click_index].info);
+        $('#github-link').attr("href", data[click_index].git);
+
+        var tech_list = '';
+        $.each(data[click_index].tech, function(index, item) {
+            tech_list += '<li class="tech-title">' + data[click_index].tech[index] + '</li>';
+
+        });
+        $('#techs').html(tech_list);
 
 
 
 
 
+    });
 
+
+    $('#overlay').click(function() {
+
+        $('#overlay').removeClass('open');
+    });
 
 
 
