5e201d8733314d944afd5e51a5f7ab218386959c
kourosh mirzaei <Koory.yellow@gmail.com>
2016-10-24 19:50:15 -0400

    media queries in progress!

--- js/writen/main.js
+++ js/writen/main.js
@@ -11,21 +11,186 @@ define(['jquery', 'background'], function($, YTPlayer) {
 
 
     //////// header animation \\\\\\\
-  
+
 
     var fadeIn = TweenMax.staggerFrom('.main-meme', 2, { scale: 0.8, opacity: 0, delay: 0.5 });
 
 
+    ///////////////////--------------- javascript code for desktop ----------- \\\\\\\\\\\\\\\\\\\\\ 
 
-    if (document.documentElement.clientWidth > 543) {
+    if (document.documentElement.clientWidth >= 1024){
 
-$("#P1").YTPlayer();
+        /////////// YOU TUBE PLAYER \\\\\\\\\\\
+        $("#P1").YTPlayer();
+             ///////sticky Pages\\\\\\\\\\\\\
+
+        var controller = new ScrollMagic.Controller();
+        // get all slides
+        var slides = document.querySelectorAll("div.panel");
+
+        // create scene for every slide
+        for (var i = 0; i < slides.length; i++) {
+            new ScrollMagic.Scene({
+                    triggerElement: slides[i],
+                    triggerHook: 0
+                })
+                .setPin(slides[i])
+                // .addIndicators()
+                .addTo(controller);
+
+        }
 
 
-//////////////////////// Scroll to \\\\\\\\\\\\\\\\
-   var target_home = $('#home').offset().top,
-        target_portfolio = $('#portfolio').offset().top,
-    target_about = $('#about').offset().top;
+
+        //////////////////////// Scroll to \\\\\\\\\\\\\\\\
+        var target_home = $('#home').offset().top,
+            target_portfolio = $('#portfolio').offset().top,
+            target_about = $('#about').offset().top;
+        // target_contact = $('#contact').offset().top;
+
+
+        //////////////////////// Scroll to \\\\\\\\\\\\\\\\
+        var target_home = $('#home').offset().top,
+            target_portfolio = $('#portfolio').offset().top,
+            target_about = $('#about').offset().top;
+
+
+
+        $("#home-link").click(function() {
+            $("html, body").animate({ scrollTop: target_home }, 1500);
+        });
+        $("#port-link").click(function() {
+            $("html, body").animate({ scrollTop: target_portfolio }, 1500);
+        });
+
+        $("#about-link").click(function() {
+            $("html, body").animate({ scrollTop: target_about }, 1500);
+        });
+       
+
+        var controller = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: 0.1, duration: "450" } });
+        new ScrollMagic.Scene({ triggerElement: "#home" })
+            .setClassToggle("#home-li", "active-me")
+            //.addIndicators() 
+            .addTo(controller);
+        new ScrollMagic.Scene({ triggerElement: "#portfolio" })
+            .setClassToggle("#portfolio-li", "active-me")
+            //.addIndicators() 
+            .addTo(controller);
+        new ScrollMagic.Scene({ triggerElement: "#about" })
+            .setClassToggle("#about-li", "active-me")
+            //.addIndicators() 
+            .addTo(controller);
+        new ScrollMagic.Scene({ triggerElement: "#about" })
+            .setClassToggle(".navbar", "active-transparent")
+            //.addIndicators() 
+            .addTo(controller);
+
+
+        //////// name fade animation \\\\\\\
+
+
+
+        var controller = new ScrollMagic.Controller();
+        var header = $('#home-wrap');
+        var abbas = TweenMax.to(header, 2, { opacity: 0, y: -75 });
+
+        var headerScene = new ScrollMagic.Scene({
+                triggerElement: '.port-head',
+                triggerHook: 0.7,
+                duration: '300'
+            })
+            .setTween(abbas)
+            //.addIndicators()
+            .addTo(controller);
+
+
+
+
+
+        //////// portfolio H1 size decrease \\\\\\\
+
+        var controller = new ScrollMagic.Controller();
+        // var smallScene = TweenMax.fromTo($('.port-head'), 4, { css: { fontSize: "5em" } }, { css: { fontSize: "3em" } });
+        var tweeny = new TimelineMax()
+            .fromTo($('.port-head'), 4, { css: { fontSize: "7em" } }, { css: { fontSize: "4em" } }, 0)
+            .to($('.port-head'), 4, { opacity: 1 }, 0);
+        var smallerScene = new ScrollMagic.Scene({
+                triggerElement: '#mobile-portfolio',
+                triggerHook: 0.6,
+                duration: '260',
+                offset: 50
+
+            })
+            .setTween(tweeny)
+            //.addIndicators({ name: "jasem" })
+            .addTo(controller);
+
+
+        // // //////// SKILLS H1 size decrease \\\\\\\
+
+        var controller = new ScrollMagic.Controller();
+        var smallScene2 = TweenMax.fromTo($('.port-head-2'), 4, { css: { fontSize: "5.5em" } }, { css: { fontSize: "3.5em" } });
+
+        var smallerScene2 = new ScrollMagic.Scene({
+                triggerElement: '.about-main',
+                triggerHook: 0.6,
+                duration: '360',
+                offset: 100
+
+            })
+            .setTween(smallScene2)
+            // .addIndicators()
+            .addTo(controller);
+
+
+
+
+
+        //////// portfolio animation \\\\\\\
+
+        var controller = new ScrollMagic.Controller();
+        var portScene = TweenMax.to($('#mobile-portfolio,#overlay-desktop'), 4, { opacity: 1, duration: 3 });
+        var headerScene = new ScrollMagic.Scene({
+                triggerElement: '.port-head',
+                triggerHook: 0.6,
+                duration: '400',
+                offset: 50
+            })
+            .setTween(portScene)
+            //.addIndicators()
+            .addTo(controller);
+
+
+
+
+
+
+        ////// portfolio END animation \\\\\\\
+
+        var controller = new ScrollMagic.Controller();
+        var portEndScene = TweenMax.to($('#projects'), 4, { opacity: 0, duration: 3, y: -80 });
+        var aboutScene = new ScrollMagic.Scene({
+                triggerElement: '.about-main',
+                triggerHook: 0.8,
+                duration: '200'
+            })
+            .setTween(portEndScene)
+           // .addIndicators()
+            .addTo(controller);
+
+
+
+///////////////////--------------- javascript code for tablet ----------- \\\\\\\\\\\\\\\\\\\\\ 
+    }else if (document.documentElement.clientWidth < 1024 && document.documentElement.clientWidth > 543) {
+
+        
+    
+
+        //////////////////////// Scroll to \\\\\\\\\\\\\\\\
+        var target_home = $('#home').offset().top,
+            target_portfolio = $('#portfolio').offset().top,
+            target_about = $('#about').offset().top;
         // target_contact = $('#contact').offset().top;
 
 
@@ -35,32 +200,36 @@ $("#P1").YTPlayer();
         $("#home-link").click(function() {
             $("html, body").animate({ scrollTop: target_home }, 1500);
         });
-    $("#port-link").click(function() {
-        $("html, body").animate({ scrollTop: target_portfolio }, 1500);
-    });
+        $("#port-link").click(function() {
+            $("html, body").animate({ scrollTop: target_portfolio }, 1500);
+        });
 
-    $("#about-link").click(function() {
-        $("html, body").animate({ scrollTop: target_about }, 1500);
-    });
-    // $("#contact-link").click(function() {
-    //     $("html, body").animate({ scrollTop: target_contact }, 1000);
-    // });
-   
-//////////////////////// Scroll  add Class \\\\\\\\\\\\\\\\
+        $("#about-link").click(function() {
+            $("html, body").animate({ scrollTop: target_about }, 1500);
+        });
+        // $("#contact-link").click(function() {
+        //     $("html, body").animate({ scrollTop: target_contact }, 1000);
+        // });
 
- var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook:0.1,duration: "450"}});
-        new ScrollMagic.Scene({triggerElement: "#home"})
-                    .setClassToggle("#home-li", "active-me") 
-                    //.addIndicators() 
-                    .addTo(controller);
-    new ScrollMagic.Scene({triggerElement: "#portfolio"})
-                    .setClassToggle("#portfolio-li", "active-me") 
-                    //.addIndicators() 
-                    .addTo(controller);
-    new ScrollMagic.Scene({triggerElement: "#about"})
-                    .setClassToggle("#about-li", "active-me") 
-                    //.addIndicators() 
-                    .addTo(controller);
+        //////////////////////// Scroll  add Class \\\\\\\\\\\\\\\\
+
+        var controller = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: 0.1, duration: "450" } });
+        new ScrollMagic.Scene({ triggerElement: "#home" })
+            .setClassToggle("#home-li", "active-me")
+            //.addIndicators() 
+            .addTo(controller);
+        new ScrollMagic.Scene({ triggerElement: "#portfolio" })
+            .setClassToggle("#portfolio-li", "active-me")
+            //.addIndicators() 
+            .addTo(controller);
+        new ScrollMagic.Scene({ triggerElement: "#about" })
+            .setClassToggle("#about-li", "active-me")
+            //.addIndicators() 
+            .addTo(controller);
+        new ScrollMagic.Scene({ triggerElement: "#about" })
+            .setClassToggle(".navbar", "active-transparent")
+            //.addIndicators() 
+            .addTo(controller);
 
 
 
@@ -92,8 +261,8 @@ $("#P1").YTPlayer();
         var controller = new ScrollMagic.Controller();
         // var smallScene = TweenMax.fromTo($('.port-head'), 4, { css: { fontSize: "5em" } }, { css: { fontSize: "3em" } });
         var tweeny = new TimelineMax()
-                .fromTo($('.port-head'), 4, { css: { fontSize: "5em" } }, { css: { fontSize: "3em" } },0)
-                .to($('.port-head'), 4, { opacity: 0},0);
+            .fromTo($('.port-head'), 4, { css: { fontSize: "5em" } }, { css: { fontSize: "3em" } }, 0)
+            .to($('.port-head'), 4, { opacity: 0 }, 0);
         var smallerScene = new ScrollMagic.Scene({
                 triggerElement: '#mobile-portfolio',
                 triggerHook: 0.6,
@@ -155,7 +324,7 @@ $("#P1").YTPlayer();
                 duration: '200'
             })
             .setTween(portEndScene)
-            //.addIndicators()
+           // .addIndicators()
             .addTo(controller);
 
 
@@ -185,92 +354,80 @@ $("#P1").YTPlayer();
 
 
         /// you tube player
-      
-        ////////////////////////////////////////////////    ////////////////////////////////////////////////
-} else {
+
+        ///////////////////--------------- javascript code for mobile ----------- \\\\\\\\\\\\\\\\\\\\\ 
+    } else {
 
 
-          //////////////////  SCROLL SPY  & smoooth Navigation  \\\\\\\\\\\\\\\\
-          // Cache selectors
-var lastId,
-    topMenu = $(".nav"),
-    topMenuHeight = topMenu.outerHeight()+15,
-    // All list items
-    menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function(){
-      var item = $($(this).attr("href"));
-      if (item.length) { return item; }
-    });
-
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
-menuItems.click(function(e){
-  var href = $(this).attr("href"),
-      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
-  $('html, body').stop().animate({ 
-      scrollTop: offsetTop
-  }, 1500);
-  e.preventDefault();
-});
-
-// Bind to scroll
-$(window).scroll(function(){
-   // Get container scroll position
-   var fromTop = $(this).scrollTop()+topMenuHeight;
-   
-   // Get id of current scroll item
-   var cur = scrollItems.map(function(){
-     if ($(this).offset().top < fromTop)
-       return this;
-   });
-   // Get the id of the current element
-   cur = cur[cur.length-1];
-   var id = cur && cur.length ? cur[0].id : "";
-   
-   if (lastId !== id) {
-       lastId = id;
-       // Set/remove active class
-       menuItems
-         .parent().removeClass("active-me")
-         .end().filter("[href='#"+id+"']").parent().addClass("active-me");
-   }                   
-});
+        var controller = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: 0.1, duration: "450" } });
+        var backWhite = TweenMax.to($('.navbar'), 4, { css:{background:'#fff'} });
+        new ScrollMagic.Scene({ triggerElement: "#about" })
+            .setTween(backWhite)
+            //.addIndicators() 
+            .addTo(controller);
 
 
+        //////////////////  SCROLL SPY  & smoooth Navigation  \\\\\\\\\\\\\\\\
+        // Cache selectors
+        var lastId,
+            topMenu = $(".nav"),
+            topMenuHeight = topMenu.outerHeight() + 15,
+            // All list items
+            menuItems = topMenu.find("a"),
+            // Anchors corresponding to menu items
+            scrollItems = menuItems.map(function() {
+                var item = $($(this).attr("href"));
+                if (item.length) {
+                    return item; }
+            });
 
+        // Bind click handler to menu items
+        // so we can get a fancy scroll animation
+        menuItems.click(function(e) {
+            var href = $(this).attr("href"),
+                offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+            $('html, body').stop().animate({
+                scrollTop: offsetTop
+            }, 1500);
+            e.preventDefault();
+        });
 
+        // Bind to scroll
+        $(window).scroll(function() {
+            // Get container scroll position
+            var fromTop = $(this).scrollTop() + topMenuHeight;
 
-   ////////////////////////////////////////////////   ////////////////////////////////////////////////
+            // Get id of current scroll item
+            var cur = scrollItems.map(function() {
+                if ($(this).offset().top < fromTop)
+                    return this;
+            });
+            // Get the id of the current element
+            cur = cur[cur.length - 1];
+            var id = cur && cur.length ? cur[0].id : "";
 
-
-   
-
-
-
-
-        //    var controller = new ScrollMagic.Controller();
-        // // get all slides
-        // var slides = document.querySelectorAll("div.panel");
-
-        // // create scene for every slide
-        // for (var i = 0; i < slides.length; i++) {
-        //     new ScrollMagic.Scene({
-        //             triggerElement: slides[i],
-        //             triggerHook: 0
-        //         })
-        //         .setPin(slides[i])
-        //          //.addIndicators()
-        //         .addTo(controller);
-        // }
+            if (lastId !== id) {
+                lastId = id;
+                // Set/remove active class
+                menuItems
+                    .parent().removeClass("active-me")
+                    .end().filter("[href='#" + id + "']").parent().addClass("active-me");
+            }
+        });
 
 
 
 
 
-        
+        ////////////////////////////////////////////////   ////////////////////////////////////////////////
 
-        ////////////////////////////////////////////////main heade disappear\\\\
+
+
+
+
+
+
+        ////////////////////////////////////////////////main head disappear\\\\
         var controller = new ScrollMagic.Controller();
         var header = $('#home-wrap');
         var abbas = TweenMax.to(header, 2, { opacity: 0, y: -80 });
@@ -291,19 +448,21 @@ $(window).scroll(function(){
         ////// portfolio H1 size decrease \\\\\\\
 
         var controller = new ScrollMagic.Controller();
-         //  var smallScene = TweenMax.fromTo($('.port-head'), 4, { css: { fontSize: "5em" } }, { css: { fontSize: "3em" } });
-         // var mobileScene  = TweenMax.to($('.port-head'), 4, { y: 80 ,opacity: 1});
-         var tween = new TimelineMax()
-                .fromTo($('.port-head'), 4, { css: { fontSize: "6em" } }, { css: { fontSize: "3em" } },0)
-                .to($('.port-head'), 4, { y: 85 ,opacity: 1},0);
+        //  var smallScene = TweenMax.fromTo($('.port-head'), 4, { css: { fontSize: "5em" } }, { css: { fontSize: "3em" } });
+        // var mobileScene  = TweenMax.to($('.port-head'), 4, { y: 80 ,opacity: 1});
+        var tween = new TimelineMax()
+            .fromTo($('.port-head'), 4, { css: { fontSize: "6em" } }, { css: { fontSize: "3em" } }, 0)
+            .to($('.port-head'), 4, { y: 85, opacity: 1 }, 0);
+            
+
         var smallerScene = new ScrollMagic.Scene({
                 triggerElement: '.port-head',
                 triggerHook: 0.7,
-                duration: '450'
+                duration: '300'
 
             })
             .setTween(tween)
-            //.addIndicators({ name: "jasem" })
+           // .addIndicators({ name: "jasem" })
             .addTo(controller);
 
 
@@ -315,7 +474,7 @@ $(window).scroll(function(){
         var smallerScene2 = new ScrollMagic.Scene({
                 triggerElement: '.about-main',
                 triggerHook: 0.6,
-                duration: '360',
+                duration: '300',
                 offset: 100
 
             })
@@ -368,14 +527,6 @@ $(window).scroll(function(){
             .addTo(controller);
 
 
-
-
-
-
-
-
-
-
     } //////////////////////////////////////////end break points!!
 
 
@@ -386,22 +537,7 @@ $(window).scroll(function(){
 
 
 
-
-
-
-    /////////////////smoooth Navigation\\\\\\\\\\\\\\\\\\\\\\
-
-
- 
-
-
-
     ///////////////// OVERLAY \\\\\\\\\\\\\\\\\\\\\\
-
-
-
-
-
 
 
     ////overlay data JSON
@@ -497,5 +633,3 @@ $(window).scroll(function(){
 
 
 });
-
-
